fc0603f84874b48ff7e03e7597770b6d2424ac54
Mark Fernandez <mark.fernandez98@gmail.com>
2017-01-04 09:03:26 +0800

    Fixed date issue

--- countdown.js
+++ countdown.js
@@ -3,10 +3,10 @@ setTimeout(function() {
 	var countdownDiv = document.getElementById("countdown-to-atchoy");
 
 	// var atchoyDate = new Date("November 26, 2022 GMT");
-	var atchoyDate = new Date("January 31, 2017 GMT");
+	var atchoyDate = new Date("January 31, 2017 08:00:00 GMT");
 	var today = new Date();
 
-	var daysLeft = Math.floor( (atchoyDate.getTime() - today.getTime()) / 86400000 );
+	var daysLeft = (atchoyDate.getTime() - today.getTime()) / 86400000;
 
 	var yearsRemaining = Math.floor(daysLeft / 365);
 	var daysRemaining = Math.floor(daysLeft % 365);
