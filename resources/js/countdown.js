58b4a174d1c7e98ce96bd140646e47e3187ce9c2
Mark Fernandez <mark.fernandez98@gmail.com>
2017-01-24 23:43:36 +0800

    Added a lot of new files and re-organized the project file structure

---
+++ resources/js/countdown.js
@@ -0,0 +1,28 @@
+setTimeout(function() {
+
+	var countdownDiv = document.getElementById("countdown-to-atchoy");
+
+	// var atchoyDate = new Date("November 26, 2022 GMT");
+	var atchoyDate = new Date("January 31, 2017 08:00:00 GMT");
+	var today = new Date();
+
+	var daysLeft = (atchoyDate.getTime() - today.getTime()) / 86400000;
+
+	var yearsRemaining = Math.floor(daysLeft / 365);
+	var daysRemaining = Math.floor(daysLeft % 365);
+
+	var yearsRemainingText = yearsRemaining > 1 ? yearsRemaining + " years" : 
+		(yearsRemaining > 0 ? yearsRemaining + " year" : "");
+
+	var daysRemainingText = daysRemaining > 1 ? daysRemaining + " days" : 
+		(daysRemaining > 0 ? daysRemaining + " day" : "");
+
+	if (yearsRemaining > 0 && daysRemaining > 0) {
+		yearsRemainingText += " and ";
+	}
+
+	var fullText = daysLeft > 0 ? "\\______" + yearsRemainingText + daysRemainingText + " to go______/" : "Get ready to party!";
+
+	countdownDiv.innerHTML = fullText;
+
+}, 1000);
